b4654cc59996303dca4872ff87b7c73ab6122433
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-08-14 15:53:23 -0400

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-// localStorage.setItem('myName', 'Linh')
-// localStorage.setItem('petName', 'Yonna')
-// localStorage.setItem('petAge', 1)
+// // localStorage.setItem('myName', 'Linh')
+// // localStorage.setItem('petName', 'Yonna')
+// // localStorage.setItem('petAge', 1)
 
-// localStorage.setItem('petAge',2)
-var myName=localStorage.getItem('myName')
-var petName= localStorage.getItem('petName')
-var petAge= localStorage.getItem('petAge')
+// // localStorage.setItem('petAge',2)
+// var myName=localStorage.getItem('myName')
+// var petName= localStorage.getItem('petName')
+// var petAge= localStorage.getItem('petAge')
 
-var myArray =[1,2,3,4]
-var myOject= { name: 'Linh', hasPet: true}
+// var myArray =[1,2,3,4]
+// var myOject= { name: 'Linh', hasPet: true}
 
-localStorage.setItem(myArray,JSON.stringify(myArray))
+// localStorage.setItem(myArray,JSON.stringify(myArray))
 
-console.log(JSON.stringify(myArray))
-console.log(JSON.stringify(myOject))
+// console.log(JSON.stringify(myArray))
+// console.log(JSON.stringify(myOject))
+
+
+
+//sellect DOM elements
+
+var newTodoForm = document.getElementById('new-todo-form')
+
+var newTodoInput =  document.getElementById('new-todo')
+
+var todosList =  document.getElementById('todos-list')
+var todos =  JSON.parse(localStorage.getItem('todos')) || []
+console.log(todos)
+
+function renderTodos() {
+    todosList.innerHTML =''
+    for( var i =0; i < todos.length; i++) {
+        var li = document.createElement('li')
+        li.setAttribute('data-index', i)
+        li.innerText = todos[i]
+        var button = document.createElement('button')
+        button.innerText = '&#10004;'
+        li.appendChild('button')
+        todosList.appendChild(li)
+    }
+}
+
+function addTodo(event) {
+    event.preventDefault()
+    var newTodoText = newTodoInput.value
+    todos.push(newTodoText)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    newTodoInput.value = ''
+    renderTodos()
+}
+function removeTodo(event){
+console.log(event.target.tagName)
+if (event.target.tagName === 'BUTTON') {
+    var inx = event.target.parentElement.getAttribute('data-index')
+    todos.splice(idx, 1)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    renderTodos()
+}
+}
+newTodoForm.addEventListener('submit', addTodo)
+renderTodos()
+todosList.addEventListener('click',removeTodo)
