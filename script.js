082f79ba9d27c0cdd7745a2d680f27cb35c7df12
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-08-14 12:27:16 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Lin')
+// localStorage.setItem('myName', 'Linh')
+// localStorage.setItem('petName', 'Yonna')
+// localStorage.setItem('petAge', 1)
+
+// localStorage.setItem('petAge',2)
+var myName=localStorage.getItem('myName')
+var petName= localStorage.getItem('petName')
+var petAge= localStorage.getItem('petAge')
+
+var myArray =[1,2,3,4]
+var myOject= { name: 'Linh', hasPet: true}
+
+localStorage.setItem(myArray,JSON.stringify(myArray))
+
+console.log(JSON.stringify(myArray))
+console.log(JSON.stringify(myOject))
